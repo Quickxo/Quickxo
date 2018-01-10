f56b9a1e6244ce47fc1f5da7f5341dcfd46ae482
Gilles Dumoulin <33807642+MVCstar@users.noreply.github.com>
2018-01-10 23:28:26 +0100

    Initial commit

---
+++ views/Error404View.php
@@ -0,0 +1,19 @@
+<?php 
+/** 
+ * @file Error404View.php
+ * HTML error 404 page of the website
+ *  
+ * @brief HTML view which displays an error 404
+ */
+?>
+<!DOCTYPE HTML>
+<html lang="en-US">
+	<head>
+		<title>404 Error</title>
+		<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+		<meta charset="UTF-8">
+	</head>
+	<body>
+		<h2>404 Error: Page not found</h2>
+	</body>
+</html>
---
+++ views/HomeView.php
@@ -0,0 +1,19 @@
+<?php 
+/** 
+ *  @file HomeView.php
+ *  HTML home page of the website
+ * 
+ *  @brief HTML view which displays the home page
+ */
+?>
+<!DOCTYPE HTML>
+<html lang="en-US">
+	<head>
+		<title>Home</title>
+		<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+		<meta charset="UTF-8">
+	</head>
+	<body>
+		<h2>Welcome to Quickxo MVC !</h2>
+	</body>
+</html>
