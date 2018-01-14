222044cbe36ddd73855f562546b35707f7d638a9
Gilles Dumoulin <33807642+MVCstar@users.noreply.github.com>
2018-01-14 18:38:28 +0100

    Initial commit

--- views/HomeView.php
+++ views/HomeView.php
@@ -6,8 +6,8 @@
  *  @brief HTML view which displays the home page
  */
 ?>
-<!DOCTYPE HTML>
-<html lang="en-US">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
 	<head>
 		<title>Home</title>
 		<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
