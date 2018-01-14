2fa14f34f78eb4a9e71fa40d0561dab61b31ba9f
Gilles Dumoulin <33807642+MVCstar@users.noreply.github.com>
2018-01-14 18:10:45 +0100

    Initial commit

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 // Front Controller design pattern
 
 // error_reporting (E_ALL);
-if (version_compare(phpversion(), '5.1.2', '<') == true) { die ('PHP5.1.2 Min.'); }
+if (version_compare(phpversion(), '5.2.0', '<') == true) { die ('PHP 5.2.0 Min.'); }
 
 // Constants:
 define ('DIRSEP', DIRECTORY_SEPARATOR);
@@ -11,7 +11,7 @@ define ('DIRSEP', DIRECTORY_SEPARATOR);
 $site_path = realpath('.' . DIRSEP) . DIRSEP;
 
 if (is_dir($site_path) == false) {
-	throw new Exception ('Invalid site path: `' . $site_path . '`');
+	die('Invalid site path: ' . $site_path);
 }
 define ('site_path', $site_path);
 
@@ -19,13 +19,13 @@ define ('site_path', $site_path);
 $config_path = $site_path . 'config' . DIRSEP; 
 
 if (is_dir($config_path) == false) {
-	throw new Exception ('Invalid config path: `' . $config_path . '`');
+	die('Invalid config path: ' . $config_path);
 }
 define ('config_path', $config_path);
 
 // include configuration file
 if(file_exists(realpath(config_path . 'Config.php')) == false) {
-	throw new Exception ('Main configuration file not found: `' . config_path . 'Config.php' . '`');
+	die('Main configuration file not found: ' . config_path . 'Config.php');
 }
 
 include(config_path . 'Config.php');
@@ -33,3 +33,5 @@ include(models_path . 'Db.php');
 include(controllers_path . 'Controller.php');
 include(commands_path . 'Command.php');
 Controller::Run();
+
+?>
